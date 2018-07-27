68291edd450ace35b31e94cdb263f9a8a37407c1
Ade Sajim <adesajim@Innovattics-Mac-mini-2.local>
2018-07-27 10:15:40 +0200

    can buy and run normal

--- TrivialDrive_v2/shared-module/src/main/java/com/asajim/billing/skulist/row/RowDataProvider.java
+++ TrivialDrive_v2/shared-module/src/main/java/com/asajim/billing/skulist/row/RowDataProvider.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2017 Google Inc. All rights reserved.
+ * Copyright 2017 Google Inc. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,10 +13,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.asajim.billing.skulist.row;
 
-apply plugin: 'com.android.instantapp'
 
 
-dependencies {
-    implementation project(':base-feature')
+/**
+ * Provider for data that corresponds to a particular row
+ */
+public interface RowDataProvider {
+    SkuRowData getData(int position);
 }
+
--- TrivialDrive_v2/wear/src/main/java/com/asajim/billingsample/TrivialDriveActivity.java
+++ TrivialDrive_v2/wear/src/main/java/com/asajim/billingsample/TrivialDriveActivity.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2017 Google Inc. All rights reserved.
+ * Copyright 2017 Google Inc. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,10 +13,17 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.asajim.billingsample;
 
-apply plugin: 'com.android.instantapp'
+import com.asajim.billing.BaseGamePlayActivity;
 
+/**
+ * Main activity that shows gas level and buttons on Android Wear
+ */
+public class TrivialDriveActivity extends BaseGamePlayActivity {
 
-dependencies {
-    implementation project(':base-feature')
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.wear_game_play;
+    }
 }
